9be069b367a521630caa19bd76f80e35dd0bdb4c
Anderson0017 <andeerson0017@gmail.com>
2020-05-02 17:00:05 -0300

    Estilizando da autenticacao

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
+import logo from '~/assets/logo.svg'
 // import { Container } from './styles';
 
 export default function SignUp() {
   return (
-    <h1>SignUp</h1>
+    <>
+      <img src={logo} alt="GoBarber"/>
+
+      <form action="">
+        <input placeholder="Nome completo"/>
+        <input type="email" placeholder="Seu e-mail"/>
+        <input type="password" placeholder="Sua senha secreta"/>
+
+        <button type="submit">Criar conta</button>
+
+        <Link to="/">Já tenho login</Link>
+
+      </form>
+    </>
   );
 }
