933127b2b2014799c4e927a2e88d4fd0624c321a
Anderson0017 <andeerson0017@gmail.com>
2020-05-03 10:31:22 -0300

    Configurando store

---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  
+}
+
+/* Recebe um state e uma action */
+//Retorna o estado alterado de acordo com a action
+export default function auth(state = INITIAL_STATE, action){
+  switch (action.type) {
+    default:
+      return state
+  }
+}
---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,4 @@
+import { all } from 'redux-saga/effects';
+
+/* Ouve as action para dispara um saga */
+export default all([]);
