f0054659f57891fbcfde1f0536e605680af3e7cd
Anderson0017 <andeerson0017@gmail.com>
2020-05-05 14:15:16 -0300

    Logout da aplicacao

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,7 +28,7 @@ export default function Header() {
               <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
-            <img src={ profile.avatar.url || "https://api.adorable.io/avatars/50/abott@adorable.png"} alt="Anderson Silva"/>
+            <img src={ profile.avatar.url || 'https://api.adorable.io/avatars/50/abott@adorable.png'} alt={profile.name}/>
           </Profile>
         </aside>
       </Content>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { signOut } from '~/store/modules/auth/actions';
+
 import { updateProfileRequest } from '~/store/modules/user/actions'
 
 import AvatarInput from './AvatarInput/index'
@@ -16,6 +18,10 @@ export default function Profile() {
 
   function handleSubmit(data){
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut(){
+    dispatch(signOut());
   }
 
   return (
@@ -35,7 +41,7 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>Sair do GoBarber</button>
     </Container>
   );
 }
